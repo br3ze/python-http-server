0a3d1b0731fc43c63827bd4badd23a0bdce9ec1f
br3ze <chamithavi@gmail.com>
2025-07-18 18:23:39 +0530

    Step 6: Serve .html files from www/ directory

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import socket
 import threading
+import os
 
 
 HOST = '127.0.0.1'
 PORT = 8080
+WEB_ROOT = '.\www'
 
 
 def parse_http_request(request_data):
@@ -19,17 +21,23 @@ def parse_http_request(request_data):
 
 def handle_response(path):
     if path == "/":
-        body = "Welcome to the homepage!"
-        return http_response(body)
-    elif path == "/hello":
-        body = "Hello Route!"
-        return http_response(body)
+        path = "/index.html"
+
+    
+    # Safely build file path
+    file_path = os.path.join(WEB_ROOT, path.lstrip("/"))
+
+    print(f"[DEBUG] File path resolved to: {file_path}")
+
+    if os.path.exists(file_path) and file_path.endswith(".html"):
+        with open(file_path, "r", encoding = "utf-8") as f:
+            body = f.read()
+        return http_response(body, content_type = "text/html")
     else:
-        body = "404 Not Found"
-        return http_response(body, status_code=404)
+        return http_response("404 Not Found", status_code = 404)
 
 
-def http_response(body, status_code=200):
+def http_response(body, status_code=200, content_type="text/plain"):
     status_messages = {
         200: "OK",
         404: "Not Found",
@@ -39,8 +47,8 @@ def http_response(body, status_code=200):
 
     response = (
         f"HTTP/1.1 {status_code} {status_text}\r\n"
-        "Content-Type: text/plain\r\n"
-        f"Content-Length: {len(body)}\r\n"
+        f"Content-Type: {content_type}\r\n"
+        f"Content-Length: {len(body.encode('utf-8'))}\r\n"
         "Connection: close\r\n"
         "\r\n"
         f"{body}"
@@ -50,7 +58,7 @@ def http_response(body, status_code=200):
 
 def handle_client(client_conn, client_addr):
     try:
-        request_bytes = client_conn.recv(1024)
+        request_bytes = client_conn.recv(4096)
         request_text = request_bytes.decode()
 
         print(f"Request from {client_addr}:\n{request_text}")
